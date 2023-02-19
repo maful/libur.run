66637ba87ebde23331d57a57241c108ff79404e0
maful <me@maful.web.id>
2023-02-19 09:18:23 +0700

    test: running chrome driver in CI

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,26 +1,49 @@
+headless_js_driver = :selenium_chrome_headless
+js_driver = :selenium_chrome
+
 Webdrivers::Chromedriver.required_version = "110.0.5481.77"
 
-Capybara.register_driver :selenium_chrome_headless_custom_browser do |app|
-  Capybara::Selenium::Driver.load_selenium
-  browser_options = ::Selenium::WebDriver::Chrome::Options.new
-  browser_options.binary = "/Applications/Brave\ Browser.app/Contents/MacOS/Brave\ Browser"
-  browser_options.args << "--headless"
-  browser_options.args << "--disable-gpu"
-  # Sandbox cannot be used inside unprivileged Docker container
-  browser_options.args << "--no-sandbox"
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
+# if CUSTOM_BROWSER is defined, should be in local development
+if ENV["CUSTOM_BROWSER"].present?
+  Capybara.register_driver :selenium_chrome_headless_custom_browser do |app|
+    Capybara::Selenium::Driver.load_selenium
+    browser_options = Selenium::WebDriver::Chrome::Options.new
+    browser_options.binary = ENV["CUSTOM_BROWSER"]
+    browser_options.add_argument("--headless")
+    browser_options.add_argument("--disable-gpu")
+    # Sandbox cannot be used inside unprivileged Docker container
+    browser_options.add_argument("--no-sandbox")
+    # Workaround https://bugs.chromium.org/p/chromedriver/issues/detail?id=2650&q=load&sort=-id&colspec=ID%20Status%20Pri%20Owner%20Summary
+    browser_options.add_argument("--disable-site-isolation-trials")
+    Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
+  end
+
+  Capybara.register_driver :selenium_chrome_custom_browser do |app|
+    Capybara::Selenium::Driver.load_selenium
+    browser_options = Selenium::WebDriver::Chrome::Options.new
+    browser_options.binary = ENV["CUSTOM_BROWSER"]
+    # Workaround https://bugs.chromium.org/p/chromedriver/issues/detail?id=2650&q=load&sort=-id&colspec=ID%20Status%20Pri%20Owner%20Summary
+    browser_options.add_argument("--disable-site-isolation-trials")
+    Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
+  end
+
+  headless_js_driver = :selenium_chrome_headless_custom_browser
+  js_driver = :selenium_chrome_custom_browser
 end
 
-default_js_driver = :selenium_chrome_headless_custom_browser
+# override configuration in CI
+if ENV["RAILS_CI"].present?
+  headless_js_driver = :selenium_chrome_headless
+  js_driver = :selenium_chrome_headless
+end
 
-Capybara.default_driver = default_js_driver
-Capybara.javascript_driver = default_js_driver
+Capybara.default_driver = headless_js_driver
+Capybara.javascript_driver = headless_js_driver
 Capybara.default_max_wait_time = 2
 
 RSpec.configure do |config|
   config.before(:each) do |example|
-    Capybara.current_driver = default_js_driver if example.metadata[:js]
-    Capybara.current_driver = :selenium_chrome if example.metadata[:selenium_chrome]
+    Capybara.current_driver = js_driver if example.metadata[:selenium_chrome]
   end
 
   config.after(:each) do
