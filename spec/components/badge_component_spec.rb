b48c1378c6b2b97caa2336fbfd982b01c1ada05a
maful <me@maful.web.id>
2023-02-27 09:58:22 +0700

    test: view component test

--- spec/components/badge_component_spec.rb
+++ spec/components/badge_component_spec.rb
@@ -3,35 +3,55 @@
 require "rails_helper"
 
 RSpec.describe(BadgeComponent, type: :component) do
-  describe "badge" do
-    let(:title) { "Verified" }
+  let(:title) { "Verified" }
 
-    it "renders default" do
-      result = render_inline(described_class.new(title:))
+  it "renders default variant" do
+    render_inline(described_class.new(title:, status: :default))
+    expect(page).to(have_css("span.badge.badge--gray.badge--medium", text: title))
+  end
 
-      content = result.at_css("span[class='badge badge--gray badge--medium']").content
-      expect(content.delete(" \t\r\n")).to(include(title))
-    end
+  it "renders error variant" do
+    render_inline(described_class.new(title:, status: :error))
+    expect(page).to(have_css("span.badge.badge--error.badge--medium", text: title))
+  end
 
-    it "renders small variant" do
-      result = render_inline(described_class.new(title:, size: :small))
+  it "renders warning variant" do
+    render_inline(described_class.new(title:, status: :warning))
+    expect(page).to(have_css("span.badge.badge--warning.badge--medium", text: title))
+  end
 
-      content = result.at_css("span[class='badge badge--gray badge--small']").content
-      expect(content.delete(" \t\r\n")).to(eq(title))
-    end
+  it "renders success variant" do
+    render_inline(described_class.new(title:, status: :success))
+    expect(page).to(have_css("span.badge.badge--success.badge--medium", text: title))
+  end
 
-    it "renders success variant" do
-      result = render_inline(described_class.new(title:, status: :success))
+  it "renders outline indigo variant" do
+    render_inline(described_class.new(title:, status: "outline-indigo"))
+    expect(page).to(have_css("span.badge.badge--outline-indigo.badge--medium", text: title))
+  end
 
-      content = result.at_css("span[class='badge badge--success badge--medium']").content
-      expect(content.delete(" \t\r\n")).to(eq(title))
-    end
+  it "renders small size" do
+    render_inline(described_class.new(title:, size: :small))
+    expect(page).to(have_css("span.badge.badge--gray.badge--small", text: title))
+  end
 
-    it "renders with custom classes" do
-      result = render_inline(described_class.new(title:, status: :success, classes: "ml-2"))
+  it "renders medium size" do
+    render_inline(described_class.new(title:))
+    expect(page).to(have_css("span.badge.badge--gray.badge--medium", text: title))
+  end
 
-      content = result.at_css("span[class='badge badge--success badge--medium ml-2']").content
-      expect(content.delete(" \t\r\n")).to(eq(title))
-    end
+  it "renders large size" do
+    render_inline(described_class.new(title:, size: :large))
+    expect(page).to(have_css("span.badge.badge--gray.badge--large", text: title))
+  end
+
+  it "renders custom attribute" do
+    render_inline(described_class.new(title:, "test-id": "random"))
+    expect(page).to(have_css("span.badge.badge--gray.badge--medium[test-id='random']", text: title))
+  end
+
+  it "renders custom classes" do
+    render_inline(described_class.new(title:, status: :success, classes: "ml-2"))
+    expect(page).to(have_css("span.badge.badge--success.badge--medium.ml-2", text: title))
   end
 end
--- spec/components/button_dropdown_component_spec.rb
+++ spec/components/button_dropdown_component_spec.rb
@@ -2,13 +2,137 @@
 
 require "rails_helper"
 
+# rubocop:disable RSpec/ExampleLength, RSpec/DescribedClass
 RSpec.describe(ButtonDropdownComponent, type: :component) do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "renders default" do
+    render_in_view_context do
+      render(ButtonDropdownComponent.new) { tag.h1("Content") }
+    end
+    expect(page).to(have_button("Actions"))
+    expect(page.find("div.button-dropdown__links")).to(have_selector("h1", text: "Content"))
+  end
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
+  it "renders custom title" do
+    render_in_view_context do
+      render(ButtonDropdownComponent.new(text: "Dropdown")) { tag.h1("Content") }
+    end
+    expect(page).to(have_button("Dropdown"))
+    expect(page.find("div.button-dropdown__links")).to(have_selector("h1", text: "Content"))
+  end
+
+  it "renders navbar type" do
+    render_in_view_context do
+      render(ButtonDropdownComponent.new(type: :navbar)) { tag.h1("Content") }
+    end
+    btn_class = ButtonDropdownComponent::BUTTON_TYPES[:navbar].gsub(" ", ".")
+    expect(page).to(have_selector("button.#{btn_class}", text: "Actions"))
+  end
+
+  it "renders filter type" do
+    render_in_view_context do
+      render(ButtonDropdownComponent.new(type: :filter)) { tag.h1("Content") }
+    end
+    btn_class = ButtonDropdownComponent::BUTTON_TYPES[:filter].gsub(" ", ".")
+    expect(page).to(have_selector("button.#{btn_class}", text: "Actions"))
+  end
+
+  it "renders filter active type" do
+    render_in_view_context do
+      render(ButtonDropdownComponent.new(type: :filter_active)) { tag.h1("Content") }
+    end
+    btn_class = ButtonDropdownComponent::BUTTON_TYPES[:filter_active].gsub(" ", ".")
+    expect(page).to(have_selector("button.#{btn_class}", text: "Actions"))
+  end
+
+  it "renders right direction" do
+    render_in_view_context do
+      render(ButtonDropdownComponent.new(direction: :right)) { tag.h1("Content") }
+    end
+    expect(page).to(have_button("Actions"))
+    direction_class = ButtonDropdownComponent::MENU_DIRECTIONS[:right].gsub(" ", ".")
+    expect(page).to(have_selector("div.button-dropdown__menu.#{direction_class}"))
+  end
+
+  it "renders left direction" do
+    render_in_view_context do
+      render(ButtonDropdownComponent.new(direction: :left)) { tag.h1("Content") }
+    end
+    expect(page).to(have_button("Actions"))
+    direction_class = ButtonDropdownComponent::MENU_DIRECTIONS[:left].gsub(" ", ".")
+    expect(page).to(have_selector("div.button-dropdown__menu.#{direction_class}"))
+  end
+
+  it "renders medium size" do
+    render_in_view_context do
+      render(ButtonDropdownComponent.new) { tag.h1("Content") }
+    end
+    expect(page).to(have_button("Actions"))
+    expect(page).to(have_selector("div.button-dropdown__menu"))
+  end
+
+  it "renders large size" do
+    render_in_view_context do
+      render(ButtonDropdownComponent.new(size: :large)) { tag.h1("Content") }
+    end
+    expect(page).to(have_button("Actions"))
+    size_class = ButtonDropdownComponent::MENU_SIZE_CLASSES[:large].gsub(" ", ".")
+    expect(page).to(have_selector("div.button-dropdown__menu.#{size_class}"))
+  end
+
+  it "renders right icon position" do
+    render_in_view_context do
+      render(ButtonDropdownComponent.new(icon_position: :right)) { tag.h1("Content") }
+    end
+    expect(page).to(have_button("Actions"))
+    icon_position_class = ButtonDropdownComponent::ICON_POSITION_CLASSES[:right].gsub(" ", ".")
+    expect(page).to(have_selector("div.button-dropdown__icon.#{icon_position_class}"))
+  end
+
+  it "renders left icon position" do
+    render_in_view_context do
+      render(ButtonDropdownComponent.new(icon_position: :left)) { tag.h1("Content") }
+    end
+    expect(page).to(have_button("Actions"))
+    icon_position_class = ButtonDropdownComponent::ICON_POSITION_CLASSES[:left].gsub(" ", ".")
+    expect(page).to(have_selector("div.button-dropdown__icon.#{icon_position_class}"))
+  end
+
+  it "renders links" do
+    render_in_view_context do
+      render(ButtonDropdownComponent.new) do |c|
+        c.with_links([
+          { name: "Login", href: "/login" },
+          { name: "Register", href: "/register" },
+        ])
+      end
+    end
+    expect(page).to(have_button("Actions"))
+    expect(page.find("div.button-dropdown__links")).to(have_selector("a.button-dropdown__link", count: 2))
+  end
+
+  it "renders with hide link" do
+    render_in_view_context do
+      render(ButtonDropdownComponent.new) do |c|
+        c.with_links([
+          { name: "Login", href: "/login", hide: true },
+          { name: "Register", href: "/register" },
+        ])
+      end
+    end
+    expect(page).to(have_button("Actions"))
+    expect(page.find("div.button-dropdown__links")).to(have_selector("a.button-dropdown__link", count: 1))
+  end
+
+  it "renders with custom attribute link" do
+    render_in_view_context do
+      render(ButtonDropdownComponent.new) do |c|
+        c.with_links([
+          { name: "Register", href: "/register", data: { turbo_method: :patch } },
+        ])
+      end
+    end
+    expect(page).to(have_button("Actions"))
+    expect(page.find("div.button-dropdown__links")).to(have_selector("a[data-turbo-method='patch']", count: 1))
+  end
 end
+# rubocop:enable RSpec/ExampleLength, RSpec/DescribedClass
