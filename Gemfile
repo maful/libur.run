fbbe25092f205d0be98955971f7e58e08ede56ec
maful <me@maful.web.id>
2023-06-20 23:56:27 +0700

    test: use cuprite instead of selenium and webdrivers

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 source "https://rubygems.org"
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
@@ -41,7 +43,7 @@ gem "hiredis", "~> 0.6.3"
 # gem "bcrypt", "~> 3.1.7"
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
-gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
+gem "tzinfo-data", platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 # Reduces boot times through caching; required in config/boot.rb
 gem "bootsnap", require: false
@@ -77,7 +79,7 @@ gem "activestorage-validator", "~> 0.2.2"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
-  gem "debug", platforms: %i[ mri mingw x64_mingw ]
+  gem "debug", platforms: [:mri, :mingw, :x64_mingw]
   gem "dotenv-rails", "~> 2.8"
   gem "rspec-rails", "~> 6.0"
   gem "factory_bot_rails", "~> 6.2"
@@ -93,9 +95,9 @@ group :test do
   gem "shoulda-matchers", "~> 5.2"
   gem "rails-controller-testing", "~> 1.0"
   gem "pundit-matchers", "~> 1.8"
-  gem "selenium-webdriver", "~> 4.8"
   gem "capybara", "~> 3.38"
-  gem "webdrivers", "~> 5.2", require: false
+  gem "cuprite", "~> 0.14.3"
+  gem "webmock", "~> 3.18"
   gem "rack_session_access", "~> 0.2"
   gem "database_cleaner-active_record", "~> 2.1"
   gem "simplecov", "~> 0.22.0", require: false
--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,54 +1,15 @@
 # frozen_string_literal: true
 
-headless_js_driver = :selenium_chrome_headless
-js_driver = :selenium_chrome
+require "capybara/cuprite"
 
-Webdrivers::Chromedriver.required_version = "110.0.5481.77"
-
-# if CUSTOM_BROWSER is defined, should be in local development
-if ENV["CUSTOM_BROWSER"].present?
-  Capybara.register_driver(:selenium_chrome_headless_custom_browser) do |app|
-    Capybara::Selenium::Driver.load_selenium
-    browser_options = Selenium::WebDriver::Chrome::Options.new
-    browser_options.binary = ENV["CUSTOM_BROWSER"]
-    browser_options.add_argument("--headless")
-    browser_options.add_argument("--disable-gpu")
-    # Sandbox cannot be used inside unprivileged Docker container
-    browser_options.add_argument("--no-sandbox")
-    # Workaround https://bugs.chromium.org/p/chromedriver/issues/detail?id=2650&q=load&sort=-id&colspec=ID%20Status%20Pri%20Owner%20Summary
-    browser_options.add_argument("--disable-site-isolation-trials")
-    Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
-  end
-
-  Capybara.register_driver(:selenium_chrome_custom_browser) do |app|
-    Capybara::Selenium::Driver.load_selenium
-    browser_options = Selenium::WebDriver::Chrome::Options.new
-    browser_options.binary = ENV["CUSTOM_BROWSER"]
-    # Workaround https://bugs.chromium.org/p/chromedriver/issues/detail?id=2650&q=load&sort=-id&colspec=ID%20Status%20Pri%20Owner%20Summary
-    browser_options.add_argument("--disable-site-isolation-trials")
-    Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
-  end
-
-  headless_js_driver = :selenium_chrome_headless_custom_browser
-  js_driver = :selenium_chrome_custom_browser
+Capybara.register_driver(:cuprite) do |app|
+  Capybara::Cuprite::Driver.new(
+    app,
+    window_size: [1400, 1400],
+    inspector: true,
+    headless: !ENV["HEADLESS"].in?(["n", "0", "no", "false"]),
+  )
 end
 
-# override configuration in CI
-if ENV["RAILS_CI"].present?
-  headless_js_driver = :selenium_chrome_headless
-  js_driver = :selenium_chrome_headless
-end
-
-Capybara.default_driver = headless_js_driver
-Capybara.javascript_driver = headless_js_driver
+Capybara.default_driver = Capybara.javascript_driver = :cuprite
 Capybara.default_max_wait_time = 4
-
-RSpec.configure do |config|
-  config.before do |example|
-    Capybara.current_driver = js_driver if example.metadata[:selenium_chrome]
-  end
-
-  config.after do
-    Capybara.use_default_driver
-  end
-end
